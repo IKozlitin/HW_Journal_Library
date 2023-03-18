f9e932921b7ffed9f66f5cd476f79f8434df3a8d
Игорь <@LAPTOP-NLBVKIFJ>
2023-03-18 19:24:36 +0300

    Add project files.

---
+++ HWmodule5/Journal.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+
+namespace Journal
+{
+    class Journal
+    {
+        public int employeeNumber { get; set; }
+                        
+        public override string ToString()
+        {
+            return $"{employeeNumber}";
+        }
+
+        public static Journal operator+(Journal j, int plus)
+        {
+            j.employeeNumber = j.employeeNumber + plus;
+            return j; 
+        }
+        public static Journal operator-(Journal j, int minus)
+        {
+            j.employeeNumber = j.employeeNumber - minus;
+            return j;
+        }
+        public override bool Equals(object obj)
+        {
+            return this.ToString() == obj.ToString();
+        }
+        public override int GetHashCode()
+        {
+            return this.ToString().GetHashCode();
+        }
+        public static bool operator ==(Journal j1, Journal j2)
+        {
+            return j1.employeeNumber == j2.employeeNumber;
+        }
+        public static bool operator !=(Journal j1, Journal j2)
+        {
+            return j1.employeeNumber != j2.employeeNumber;
+        }
+        public static bool operator >(Journal j1, Journal j2)
+        {
+            return (j1.employeeNumber > j2.employeeNumber);
+        }
+        public static bool operator <(Journal j1, Journal j2)
+        {
+            return (j1.employeeNumber < j2.employeeNumber);
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Journal j1 = new Journal {employeeNumber = 50};
+            Journal j2 = new Journal {employeeNumber = 100};
+            WriteLine($"Количество сотрудников:\n 1jour = {j1} 2jour = {j2}");
+
+            WriteLine("------------------------------");
+
+            Write("Принято сотрудников jour1: ");
+            int plus = int.Parse(ReadLine());
+            WriteLine($"Стало jour1: {j1+plus}");
+
+            WriteLine("------------------------------");
+
+            Write("Уволено сотрудников jour1: ");
+            int minus = int.Parse(ReadLine());
+            WriteLine($"Стало jour1: {j1-minus}");
+
+            WriteLine("------------------------------");
+           
+            WriteLine($"{j1} == {j2}: {(j1==j2)}");
+            WriteLine($"{j1} != {j2}: {(j1!=j2)}");
+
+            WriteLine("------------------------------");
+
+            WriteLine($"{j1} > {j2}: {(j1>j2)}");
+            WriteLine($"{j1} < {j2} : {(j1<j2)}");
+
+        }
+    }
+}
---
+++ HWmodule5/Library.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+
+namespace Library
+{
+    class Book
+    {
+      public string BookName { get; set; }
+        public override string ToString()
+        {
+            return BookName.ToString();
+        }
+    }
+    class Library
+    {
+        Book[] bookArr;
+        public Library(int size)
+        {
+            bookArr = new Book[size];
+        }
+        public int Length
+        { 
+            get { return bookArr.Length; } 
+        }
+        public Book this[int index]
+        {
+            get
+            {
+                if (index >= 0 && index < bookArr.Length)
+                {
+                    return bookArr[index];
+                }
+                throw new IndexOutOfRangeException();
+            }
+            set
+            {
+                bookArr[index] = value;
+            }
+        }
+        public int FindByName(string name)
+        {
+            for (int i = 0; i < bookArr.Length; i++)
+            {
+                if (bookArr[i].BookName == name)
+                {
+                    return i;
+                }
+            }
+            return -1;
+        }
+        public Book this[string name]
+        {
+            get
+            {
+               return this[FindByName(name)];
+            }
+            set 
+            {
+               bookArr[0] = value; 
+            }
+        }
+       
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Library book = new Library(3);
+            book[0] = new Book
+            {
+                BookName = "Война и мир"
+            }; 
+           
+            book[1] = new Book
+            {
+                BookName = "Золотой телёнок"
+            }; 
+           
+            book[2] = new Book
+            {
+                BookName = "Горе от ума"
+            };
+
+            try
+            {
+                for (int i = 0; i < book.Length; i++)
+                {
+                    WriteLine(book[i]);
+                }
+                WriteLine();
+                
+                WriteLine($"Название книги: {book["Горе от ума"]}");
+                WriteLine($"Название книги: {book["Война и мир"]}");
+                WriteLine($"Название книги: {book["Золотой телёнок"]}");
+                WriteLine($"Название книги: {book["Капитанская дочка"]}");
+
+            }
+            catch (Exception ex)
+            {
+                WriteLine(ex.Message);
+            }
+        }
+    }
+}
